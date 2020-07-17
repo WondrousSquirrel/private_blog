0e19ed9ce80322004a37bbf0213377087aaef383
Alexander <wondroussquirre@gmail.com>
2020-07-17 18:47:35 +0300

    Добавлена форма регистрации

---
+++ client/src/Components/Register/Register.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import * as yup from 'yup';
+import PropTypes from 'prop-types';
+import { withFormik, Form, Field } from "formik";
+
+const Register = ({ errors, touched }) => {
+  return (
+    <>
+      <Form>
+        <div className="fields">
+          <label htmlFor="name">Имя</label>
+          <Field name="name" type="text" />
+          {touched.name && errors.name && <div className="errors">{errors.name}</div>}
+        </div>
+        <div className="fields">
+          <label htmlFor="email">Электроный адрес</label>
+          <Field name="email" type="text" />
+          {touched.email && errors.email && <div className="errors">{errors.email}</div>}
+        </div>
+        <div className="fields">
+          <label htmlFor="password">Пароль</label>
+          <Field name="password" type="password" />
+          {touched.password && errors.password && <div className="errors">{errors.password}</div>}
+        </div>
+        <div className="fields">
+          <label htmlFor="rePassword">Повторите пароль</label>
+          <Field name="rePassword" type="password" />
+          {touched.rePassword && errors.rePassword &&
+            <div className="errors">{errors.rePassword}</div>}
+        </div>
+        <button type="submit">Регистрация</button>
+      </Form>
+    </>
+  );
+};
+
+
+const validatedRegister = withFormik({
+  mapPropsToValues() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      rePassword: "",
+    };
+  },
+  validationSchema:yup.object({
+    name: yup
+      .string()
+      .max(20, "Имя должно быть длиною в 20 символов или меньше")
+      .required("Поле обязательное"),
+    email: yup
+      .string()
+      .email("Не правильно введен электронный адрес")
+      .required("Поле обязательное"),
+    password: yup
+      .string()
+      .min(8, "Пароль должен быть длиною не менее 8 символов")
+      .max(16, "Пароль не должен привышать длину в 16 символов")
+      .required("Поле обязательное"),
+    rePassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Пароли не совпадают")
+      .required("Поле обязательное"),
+  }),
+  handleSubmit(values, { props, resetForm, setSubmitting }) {
+    console.log(values);
+    resetForm();
+  },
+})(Register);
+
+
+Register.propTypes = {
+  errors: PropTypes.object.isRequired,
+  touched: PropTypes.object.isRequired,
+};
+
+export default validatedRegister;
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Route } from 'react-router';
+import React, { Suspense, lazy} from 'react';
+import { Route, Switch } from 'react-router';
+
+
+const Register = lazy(() => import('./Components/Register/Register'));
 
 const Home = () => <h1>Home</h1>;
 
-const createRoutes = () => (
-  <Route exact path='/' component={Home}/>
-);
+const createRoutes = () => <Switch>
+  <Suspense fallback={<div>Загрузка...</div>}>
+    <Route exact path='/' component={Home}/>
+    <Route path='/register' component={Register}/>
+  </Suspense>
+</Switch>;
 
 const routes = createRoutes();
 
