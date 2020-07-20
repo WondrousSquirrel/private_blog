3d822827ca07164066bcd478e368a63a58883f8a
Alexander <wondroussquirre@gmail.com>
2020-07-20 12:30:32 +0300

    Окружение для клиентской части

---
+++ client/src/config/appConfig.js
@@ -0,0 +1,8 @@
+import ENVIRONMENT from "./environment";
+
+
+
+const endpoint = ENVIRONMENT === 'production' ?
+  'http://node-env.eba-h2rsrmam.eu-central-1.elasticbeanstalk.com/' : 'http://127.0.0.1:5000/';
+
+export default endpoint;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -12,6 +12,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootSaga from "./rootSaga";
 import rootReducer from "./rootReducer";
 import authReducer from "../reducers/authReducer";
+import { ENVIRONMENT } from "../config/environment";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -21,12 +22,17 @@ const persistConfig = {
   whitelist: [authReducer] // список редьюсеров которые необходимо хранить
 };
 
-const pReducer = persistReducer(persistConfig, rootReducer);
-const store = createStore(
-  pReducer,
+const devTools = ENVIRONMENT === 'production' ?
+  applyMiddleware(sagaMiddleware) :
   compose(applyMiddleware(sagaMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(
+  pReducer, devTools  
 );
+
 const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
