fa08dc02095328f12d654214132586e40902e0f8
Alexander <wondroussquirre@gmail.com>
2020-07-21 12:29:01 +0300

    Добавлен Login компонент

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,8 +1,8 @@
 import React, { Suspense, lazy} from 'react';
 import { Route, Switch } from 'react-router';
 
-
-const Register = lazy(() => import('./Components/Register/Register'));
+const Register = lazy(() => import('./Components/Auth/Register'));
+const Login = lazy(() => import('./Components/Auth/Login'));
 
 const Home = () => <h1>Home</h1>;
 
@@ -10,6 +10,7 @@ const createRoutes = () => <Switch>
   <Suspense fallback={<div>Загрузка...</div>}>
     <Route exact path='/' component={Home}/>
     <Route path='/register' component={Register}/>
+    <Route path='/login' component={Login}/>
   </Suspense>
 </Switch>;
 
