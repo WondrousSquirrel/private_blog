b7c018e29c3de11d60aae6f44f7887de49bf3f46
Alexander <wondroussquirre@gmail.com>
2020-07-22 17:48:24 +0300

    страница профиля

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
-const Profile = () => <h1>Profile</h1>;
+import avatar from './avatar.png';
+
+const Profile = () => <div className='profile-wrapper'>
+  <div className="profile">
+    <div className="avatar-wrapper">
+      <img src={avatar} className='profile-avatar'/>
+    </div>
+    <div className="profile-buttons">
+      <Button className='edit-profile-button'>Редактировать</Button>
+      <Button className='delete-user-button'>Удалить</Button>
+    </div> 
+  </div>
+</div>;
 
 export default Profile;
